dfa5bc8d0614a5b88866562aa3dfac1ff5edc7ac
Antonio Ettorre <antonio.ettorre@globalquakemodel.org>
2022-10-13 11:54:28 +0200

    add if also on build jpeg

--- multibuild/library_builders.sh
+++ multibuild/library_builders.sh
@@ -155,7 +155,7 @@ function build_jpeg {
     (cd jpeg-${JPEG_VERSION} \
         && ./configure --prefix=$BUILD_PREFIX \
         && make -j4 \
-        && sudo make install)
+        && if [ -n "$IS_OSX" ]; then sudo make install; else make install; fi)
     touch jpeg-stamp
 }
 
