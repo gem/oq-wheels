c2fd2a126529546c901b45fd78ae1ced3e7fd5bc
Antonio Ettorre <antonio.ettorre@globalquakemodel.org>
2022-10-14 11:06:11 +0200

    typo on if syntax

--- env_vars.sh
+++ env_vars.sh
@@ -25,7 +25,7 @@ export SETUPTOOLS_USE_DISTUTILS=stdlib
 if [[ "$REPO_DIR" == "pyproj" ]]; then
  export PROJ_DIR=${GITHUB_WORKSPACE}/pyproj/pyproj/proj_dir
  export PROJ_DATA=${PROJ_DIR}/share/proj
- if [  "$IS_OSX" ]; then 
+ if [ -z "$IS_OSX" ]; then 
 	 echo "PROJ_DIR on ML2014  "
      export PROJ_DIR=/io/pyproj/pyproj/proj_dir
      export PROJ_DATA=${PROJ_DIR}/share/proj
