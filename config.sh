c76c04839c7b32205583489f46f41c28932cfa2e
antonioettorre <antonio.ettorre@globalquakemodel.org>
2023-11-10 11:38:58 +0100

    update cmake section for gdal

--- config.sh
+++ config.sh
@@ -201,33 +201,33 @@ function build_gdal {
 		cd build \
 		# build using cmake
         cmake .. \
-            -DCMAKE_INSTALL_PREFIX=$GDAL_DIR \
-            -DBUILD_SHARED_LIBS=ON \
-            -DCMAKE_BUILD_TYPE=Release \
-            -DGDAL_BUILD_OPTIONAL_DRIVERS=OFF \
-            -DGDAL_ENABLE_DRIVER_MBTILES=OFF \
-            -DOGR_BUILD_OPTIONAL_DRIVERS=OFF \
-            -DOGR_ENABLE_DRIVER_CSV=ON \
-            -DOGR_ENABLE_DRIVER_DGN=ON \
-            -DOGR_ENABLE_DRIVER_DXF=ON \
-            -DOGR_ENABLE_DRIVER_FLATGEOBUF=ON \
-            -DOGR_ENABLE_DRIVER_GEOJSON=ON \
-            -DOGR_ENABLE_DRIVER_GML=ON \
-            -DOGR_ENABLE_DRIVER_GMT=ON \
-            -DOGR_ENABLE_DRIVER_GPKG=ON \
-            -DOGR_ENABLE_DRIVER_GPX=ON \
-            -DOGR_ENABLE_DRIVER_OPENFILEGDB=ON \
-            -DGDAL_ENABLE_DRIVER_PCIDSK=ON \
-            -DOGR_ENABLE_DRIVER_S57=ON \
-            -DOGR_ENABLE_DRIVER_SHAPE=ON \
-            -DOGR_ENABLE_DRIVER_SQLITE=ON \
-            -DOGR_ENABLE_DRIVER_TAB=ON \
-            -DOGR_ENABLE_DRIVER_VRT=ON \
-            -DBUILD_CSHARP_BINDINGS=OFF \
-            -DBUILD_PYTHON_BINDINGS=OFF \
-            -DBUILD_JAVA_BINDINGS=OFF \
-        && cmake --build . -j4 \
-        && if [ -n "$IS_OSX" ]; then sudo cmake --install .; else cmake --install .; fi)
+        -DCMAKE_INSTALL_PREFIX=$GDAL_DIR \
+        -DBUILD_SHARED_LIBS=ON \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DGDAL_BUILD_OPTIONAL_DRIVERS=OFF \
+        -DGDAL_ENABLE_DRIVER_MBTILES=OFF \
+        -DOGR_BUILD_OPTIONAL_DRIVERS=OFF \
+        -DOGR_ENABLE_DRIVER_CSV=ON \
+        -DOGR_ENABLE_DRIVER_DGN=ON \
+        -DOGR_ENABLE_DRIVER_DXF=ON \
+        -DOGR_ENABLE_DRIVER_FLATGEOBUF=ON \
+        -DOGR_ENABLE_DRIVER_GEOJSON=ON \
+        -DOGR_ENABLE_DRIVER_GML=ON \
+        -DOGR_ENABLE_DRIVER_GMT=ON \
+        -DOGR_ENABLE_DRIVER_GPKG=ON \
+        -DOGR_ENABLE_DRIVER_GPX=ON \
+        -DOGR_ENABLE_DRIVER_OPENFILEGDB=ON \
+        -DGDAL_ENABLE_DRIVER_PCIDSK=ON \
+        -DOGR_ENABLE_DRIVER_S57=ON \
+        -DOGR_ENABLE_DRIVER_SHAPE=ON \
+        -DOGR_ENABLE_DRIVER_SQLITE=ON \
+        -DOGR_ENABLE_DRIVER_TAB=ON \
+        -DOGR_ENABLE_DRIVER_VRT=ON \
+        -DBUILD_CSHARP_BINDINGS=OFF \
+        -DBUILD_PYTHON_BINDINGS=OFF \
+        -DBUILD_JAVA_BINDINGS=OFF 
+        cmake --build . -j4 
+        if [ -n "$IS_OSX" ]; then sudo cmake --install .; else cmake --install .; fi)
     if [ -n "$IS_OSX" ]; then
         :
     else
